4b2b94e29fd173f05cab14bedc00cc0cfa18e062
SOKONG SOK <100309426+Jeff-kxng@users.noreply.github.com>
2023-04-18 13:29:02 +0700

    Datastructures

--- src/Dynamic_Array/array.java
+++
@@ -1,7 +0,0 @@
-package Dynamic_Array;
-
-public class array {
-    public static void main(String[] args) {
-
-    }
-}
--- src/Linear_search/Main.java
+++ src/Linear_search/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // linear search = Iterate through a collection one element at a time
         int[] array = {9, 1, 8, 2, 7, 3, 6, 4, 5};
         int index = linearSearch(array, 10);
-        if (index != 1){
+        if (index != -1){
             System.out.println("Element found at index : " + index);
         } else {
             System.out.println("Element can not found at index");
