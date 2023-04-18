a13c650e68bcfd58a3f3f8e1978df1a073b5e7ee
SOKONG SOK <100309426+Jeff-kxng@users.noreply.github.com>
2023-04-18 13:29:36 +0700

    Jeff

---
+++ src/Adjacencylist/Main.java
@@ -0,0 +1,9 @@
+package Adjacencylist;
+
+public class Main {
+    // Adjacency = An array / arrayList of linkedlists
+    // each linkedlists has a unique nosde at the head
+    // all adjacent neighbors to that node are added to that node's linkedlist
+    // runtime complexity to check an edge : 0(v)
+    // space complexity : 0 ( v + e)
+}
